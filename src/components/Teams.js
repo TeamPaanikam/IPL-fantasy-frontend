9cb95b6b4335a2ba87516b5be161ef50ccd33750
Azim Javed <thatazimjaved@gmail.com>
2020-10-14 19:25:21 +0530

    Fix alpha

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export default function Teams(props) {
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
-    useEffect(()=>{
-        props.teams.forEach(team => {
+    let [teams, setTeams] = useState([[{ players: [] }]])
+    useEffect(() => {
+        let _teams = props.teams
+        _teams.forEach(team => {
             team.players.sort();
         });
-    })
+        setTeams(_teams);
+    }, [props.teams])
     return (
         <div>
             <h1>Teams</h1>
             {
-                props.teams.map((team) => {
+                teams.map((team) => {
                     if (team.sattaLagaDiya) {
                         return (<div> <h4>{capitalizeFirstLetter(team.username.split('@')[0])} </h4><p>{team.players.map((player, index) => index === team.players.length - 1 ? (player) : (player + ", "))}</p></div>)
                     }
